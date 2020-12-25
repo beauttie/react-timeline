8bf497b5ad8b37d6acda412254564deb4d5a5e7d
beauttie <bak02013@pomona.edu>
2020-12-25 03:56:14 -0800

    Add PropTypes to Timeline and TimelineEvent

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+import PropTypes from 'prop-types';
 
 const Timeline = (props) => {
   const timelineComponents = props.events.map((event, i) => {
@@ -20,5 +21,9 @@ const Timeline = (props) => {
     </div>
   );
 }
+
+Timeline.propTypes = {
+  events: PropTypes.array.isRequired
+};
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import PropTypes from 'prop-types';
 
 const TimelineEvent = (props) => {
   
   return (
     <div className="timeline-event">
-      <span className="event-person">{props.person}</span>
-      <span className="event-status">{props.status}</span>
+      <p className="event-person">{props.person}</p>
+      <p className="event-status">{props.status}</p>
       <Timestamp className="event-time" time={props.timeStamp} />
     </div>
   );
 }
+
+TimelineEvent.propTypes = {
+  person: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired
+};
 
 export default TimelineEvent;
